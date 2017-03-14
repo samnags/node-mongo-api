95674d2e3131cfb925da844e4abb2bf0c4d8b395
samnags <sam.nagourney@gmail.com>
2017-03-14 16:06:21 -0700

    Added GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,12 +7,20 @@
   const { app } = require('./../server')
   const { Todo }= require('./../models/todo')
 
+  const todos = [{
+    text: '1st Test To Do'
+  }, {
+    text: '2nd Test To Do'
+  }]
+
+
 // handling the database
 beforeEach((done) => {
   // will wipe all of todos
   Todo.remove({}).then(() => {
-    done()
-  })
+    return Todo.insertMany(todos);
+    // using expression syntax
+  }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -36,7 +44,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done()
@@ -56,11 +64,23 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done()
         }).catch((e) => {
           done(e)
         })
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done)
   })
 })
